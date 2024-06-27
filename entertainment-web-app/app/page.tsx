9cdb936675a7ab4efb42942eed41681a66a3221a
kaikaci12 <somewonesomewone@gmail.com>
2024-06-27 22:24:15 +0400

    add nav buttons toggling

--- entertainment-web-app/app/page.tsx
+++ entertainment-web-app/app/page.tsx
@@ -4,9 +4,11 @@ import data from "../data.json";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [movieData, setMovieData] = useState([]);
-
-  const [seriesData, setSeriesData] = useState([]);
+  const [movieData, setMovieData] = useState(data);
+  const [seriesActive, setSeriesActive] = useState(false);
+  const [moviesActive, setMoviesActive] = useState(false);
+  const [bookmarkedActive, setBookMarkActive] = useState(false);
+  const [homeActive, setHomeActive] = useState(true);
   const trendingData = data.filter((movie) => movie.isTrending);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   useEffect(() => {
@@ -45,28 +47,74 @@ export default function Home() {
           />
         </svg>
         <nav className="flex gap-[20px] sm:gap-[40px] items-center lg:flex-col">
-          <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+          <svg
+            className="cursor-pointer"
+            onClick={() => {
+              setHomeActive(true);
+
+              setMoviesActive(false);
+
+              setBookMarkActive(false);
+              setSeriesActive(false);
+            }}
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
               d="M8 0H1C.4 0 0 .4 0 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1Zm0 11H1c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1v-7c0-.6-.4-1-1-1ZM19 0h-7c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1Zm0 11h-7c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1v-7c0-.6-.4-1-1-1Z"
-              fill={`#5A698F`}
+              fill={` ${homeActive ? "#fff" : " #5A698F"}`}
             />
           </svg>
-          <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+          <svg
+            className="cursor-pointer"
+            onClick={() => {
+              setMoviesActive(true);
+              setHomeActive(false);
+              setBookMarkActive(false);
+              setSeriesActive(false);
+            }}
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
               d="M16.956 0H3.044A3.044 3.044 0 0 0 0 3.044v13.912A3.044 3.044 0 0 0 3.044 20h13.912A3.044 3.044 0 0 0 20 16.956V3.044A3.044 3.044 0 0 0 16.956 0ZM4 9H2V7h2v2Zm-2 2h2v2H2v-2Zm16-2h-2V7h2v2Zm-2 2h2v2h-2v-2Zm2-8.26V4h-2V2h1.26a.74.74 0 0 1 .74.74ZM2.74 2H4v2H2V2.74A.74.74 0 0 1 2.74 2ZM2 17.26V16h2v2H2.74a.74.74 0 0 1-.74-.74Zm16 0a.74.74 0 0 1-.74.74H16v-2h2v1.26Z"
-              fill="#5A698F"
+              fill={`${moviesActive ? "#fff" : "#5A698F"}`}
             />
           </svg>
-          <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg">
+          <svg
+            className="cursor-pointer"
+            onClick={() => {
+              setSeriesActive(true);
+              setMoviesActive(false);
+              setHomeActive(false);
+              setBookMarkActive(false);
+            }}
+            width="20"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
               d="M20 4.481H9.08l2.7-3.278L10.22 0 7 3.909 3.78.029 2.22 1.203l2.7 3.278H0V20h20V4.481Zm-8 13.58H2V6.42h10v11.64Zm5-3.88h-2v-1.94h2v1.94Zm0-3.88h-2V8.36h2v1.94Z"
-              fill="#5A698F"
+              fill={`${seriesActive ? "#fff" : "#5A698F"}`}
             />
           </svg>
-          <svg width="17" height="20" xmlns="http://www.w3.org/2000/svg">
+          <svg
+            className="cursor-pointer"
+            onClick={() => {
+              setBookMarkActive(true);
+              setSeriesActive(false);
+              setMoviesActive(false);
+              setHomeActive(false);
+            }}
+            width="17"
+            height="20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <path
               d="M15.387 0c.202 0 .396.04.581.119.291.115.522.295.694.542.172.247.258.52.258.82v17.038c0 .3-.086.573-.258.82a1.49 1.49 0 0 1-.694.542 1.49 1.49 0 0 1-.581.106c-.423 0-.79-.141-1.098-.423L8.46 13.959l-5.83 5.605c-.317.29-.682.436-1.097.436-.202 0-.396-.04-.581-.119a1.49 1.49 0 0 1-.694-.542A1.402 1.402 0 0 1 0 18.52V1.481c0-.3.086-.573.258-.82A1.49 1.49 0 0 1 .952.119C1.137.039 1.33 0 1.533 0h13.854Z"
-              fill="#5A698F"
+              fill={`${bookmarkedActive ? "#fff" : "#5A698F"}`}
             />
           </svg>
         </nav>
@@ -76,7 +124,7 @@ export default function Home() {
           className="w-[24px] h-[24px] sm:w-[32px] sm:h-[32px] rounded-full border-2 border-white"
         />
       </header>
-      <main className="p-[16px] flex flex-col gap-[24px] ">
+      <main className="p-[16px] flex flex-col gap-[24px] overflow-hidden">
         <div className="flex gap-[24px] mt-[26px] sm:mt-[34px] lg:mt-[45px]">
           <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg">
             <path
@@ -93,8 +141,8 @@ export default function Home() {
         <h1 className="text-[#FFF] text-[20px] font-normal tracking-[-0.312px]">
           Trending
         </h1>
-        <div className="overflow-x-auto w-full relative">
-          <section className="flex  w-full lg:gap-[40px] gap-[16px] absolute  py-[10px] ">
+        <div className=" w-full ">
+          <section className="flex  w-full lg:gap-[40px] gap-[16px] overflow-x-scroll  py-[10px] ">
             {trendingData.map((movie, index) => {
               return (
                 <div
