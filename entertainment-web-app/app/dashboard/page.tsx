1fbd44c423e51eeec3e82b6a31986c7bbc7ecb0a
kaikaci12 <somewonesomewone@gmail.com>
2024-07-01 17:53:02 +0400

    fix search bar searching according to categories

--- entertainment-web-app/app/dashboard/page.tsx
+++ entertainment-web-app/app/dashboard/page.tsx
@@ -15,7 +15,7 @@ export default function Dashboard() {
   const [moviesActive, setMoviesActive] = useState(false);
   const [bookmarkedActive, setBookMarkActive] = useState(false);
   const [homeActive, setHomeActive] = useState(true);
-  const trendingData = data.filter((movie) => movie.isTrending);
+
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [logOut, setLogOut] = useState(false);
   const [searchValue, setSearchValue] = useState("");
@@ -25,7 +25,7 @@ export default function Dashboard() {
   const filterMoviesByCategory = (category: string) => {
     setFilteredMovies(filmData.filter((movie) => movie.category === category));
   };
-
+  const trendingData = filmData.filter((movie) => movie.isTrending);
   const router = useRouter();
   const isUserLoggedIn = useState(false);
   useEffect(() => {
@@ -60,16 +60,29 @@ export default function Dashboard() {
     if (seriesActive) return "TV Series";
   }
   useEffect(() => {
-    const results = filmData.filter((value) =>
-      value.title.toLowerCase().includes(searchValue.toLowerCase().trim())
+    setSearchResults(
+      filmData.filter((value) => {
+        const results = value.title
+          .toLowerCase()
+          .includes(searchValue.toLowerCase().trim());
+
+        if (homeActive) {
+          return results;
+        } else if (moviesActive) {
+          return results && value.category === "Movie";
+        } else if (seriesActive) {
+          return results && value.category === "TV Series";
+        } else {
+          return results && value.isBookmarked;
+        }
+      })
     );
-    setSearchResults(results);
   }, [searchValue]);
   console.log(searchResults);
 
   return (
     <div className="  bg-[#10141E]  w-full h-full lg:gap-[30px]  lg:flex   sm:p-[25px] lg:py-[32px] ">
-      <header className="sm:h-[72px] w-full h-[56px] bg-[#161D2F] sm:rounded-[10px] lg:rounded-[20px] flex justify-between items-center px-[16px]  lg:h-screen  lg:w-[96px] lg:flex-col lg:py-[32px] ">
+      <header className="sm:h-[72px]  w-full h-[56px] bg-[#161D2F] sm:rounded-[10px] lg:rounded-[20px] flex justify-between items-center px-[16px]  lg:h-screen  lg:w-[96px] lg:flex-col lg:py-[32px] ">
         <svg
           className=""
           xmlns="http://www.w3.org/2000/svg"
@@ -197,12 +210,12 @@ export default function Dashboard() {
             <section
               className={`  overflow-x-auto py-10 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-[1000px] `}
             >
-              <div className="flex flex-nowrap gap-[30px] lg:gap-[70px] p-1 lg:p-0  ">
+              <div className=" flex flex-nowrap gap-[30px] lg:gap-[70px] p-1 lg:p-0  ">
                 {trendingData.map((movie, index) => {
                   return (
                     <div key={index} className="relative flex-shrink-0">
                       <div
-                        className=" lg:w-[470px] w-[320px] group  h-[140px] md:w-[470px] md:h-[230px] rounded-[8px] flex flex-col items-center gap-[4px] p-[16px] "
+                        className="movie-container lg:w-[470px] w-[320px] group  h-[140px] md:w-[470px] md:h-[230px] rounded-[8px] flex flex-col items-center gap-[4px] p-5 "
                         style={{
                           backgroundImage: `url(${
                             windowWidth >= 1024
@@ -258,26 +271,23 @@ export default function Dashboard() {
                               })
                             );
                           }}
-                          className="cursor-pointer absolute right-[10px] top-[10px] z-[999]"
+                          className={`cursor-pointer absolute right-[10px] ${
+                            movie.isBookmarked
+                              ? "hover:bg-black"
+                              : "hover:bg-white"
+                          }  rounded-full    top-[10px] z-[999]`}
                         >
+                          <circle fill="#10141E"></circle>
                           <svg
-                            className=""
                             xmlns="http://www.w3.org/2000/svg"
                             width="32"
                             height="32"
                             viewBox="0 0 32 32"
-                            fill={`${movie.isBookmarked ? "#fff" : "#10141E"}`}
+                            fill={`${movie.isBookmarked ? "#fff" : "none"}`}
                           >
-                            <circle
-                              opacity="0.500647"
-                              cx="16"
-                              cy="16"
-                              r="16"
-                              fill="#10141E"
-                            />
                             <path
                               d="M20.7112 9.771L20.7215 9.77548L20.7319 9.77965C20.7992 9.80657 20.8386 9.84049 20.8705 9.88692C20.9032 9.93458 20.9167 9.97786 20.9167 10.0364V21.9636C20.9167 22.0221 20.9032 22.0654 20.8705 22.1131C20.8386 22.1595 20.7992 22.1934 20.7319 22.2203L20.7237 22.2236L20.7156 22.2271C20.7107 22.2292 20.6807 22.2407 20.6094 22.2407C20.5085 22.2407 20.4397 22.2142 20.3686 22.15L16.3572 18.2346L15.8333 17.7233L15.3095 18.2346L11.2975 22.1505C11.2129 22.2276 11.1421 22.25 11.0573 22.25C11.02 22.25 10.9882 22.2433 10.9555 22.229L10.9452 22.2245L10.9347 22.2203C10.8674 22.1934 10.8281 22.1595 10.7962 22.1131C10.7635 22.0654 10.75 22.0221 10.75 21.9636V10.0364C10.75 9.97786 10.7635 9.93458 10.7962 9.88692C10.8281 9.84049 10.8674 9.80657 10.9347 9.77965L10.9452 9.77548L10.9555 9.771C10.9882 9.75674 11.02 9.75 11.0573 9.75H20.6094C20.6466 9.75 20.6784 9.75674 20.7112 9.771Z"
-                              stroke="white"
+                              stroke={movie.isBookmarked ? "white" : "black"}
                               stroke-width="1.5"
                             />
                           </svg>
